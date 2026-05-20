a81e5ab2bbf19b316b8e3574d1ea59332c3ecc51
HK7470 <ASUS@LAPTOP-VIEOFEFH>
2026-05-20 18:46:27 +0800

    originalExpansion 0-0-1

---
+++ src/main/java/com/g7470/originalexpansion/OriginalExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.g7470.originalexpansion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OriginalExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
