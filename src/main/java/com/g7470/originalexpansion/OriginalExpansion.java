092b6884096a6de6dfdf27db075addb94e81fe37
HK7470 <ASUS@LAPTOP-VIEOFEFH>
2026-05-23 22:27:54 +0800

    originalExpansion 0-0-2

--- src/main/java/com/g7470/originalexpansion/OriginalExpansion.java
+++ src/main/java/com/g7470/originalexpansion/OriginalExpansion.java
@@ -1,5 +1,6 @@
 package com.g7470.originalexpansion;
 
+import com.g7470.originalexpansion.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class OriginalExpansion implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		ModItems.register();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
